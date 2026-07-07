1a4fa3c9d954631a47ac33fce09fd7c58aecb799
FabioSilva11 <produtorfabiosilva@gmail.com>
2026-07-07 18:22:19 -0400

    f fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import 'gen_l10n/app_localizations.dart';
 import 'core/ads/ad_manager.dart';
 import 'core/api/streambert_api.dart';
 import 'core/api/tomato_api.dart';
